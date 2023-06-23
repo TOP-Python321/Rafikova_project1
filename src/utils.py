ee27491d092d84efaa583cc999213b817867b318
LiliyaRafikova <liliya89fm@mail.ru>
2023-06-23 13:00:31 +0500

    Добавление функции read_players(): чтение файла players.ini

--- src/utils.py
+++ src/utils.py
@@ -2,4 +2,19 @@
 Вспомогательные функции.
 """
 
+from configparser import ConfigParser
+import data
+def read_players() -> bool:
+    """Читает файл данных игроков, сохраняет информацию в соответствующую глобальную структуру данных. Возвращает True, если в файле данных игроков есть хотя бы одна запись, иначе False."""
+    config = ConfigParser()
+    config.read(data.PLAYERS_PATH)
+    config = {
+        player_name: {
+            key: int(value)
+            for key, value in config[player_name].items()
+        }
+        for player_name in config.sections()
+    }
+    data.players_db = config
+    return bool(config)
 
