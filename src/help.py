b723087b6a317d22dc952c7ff34d75ffcbcc966a
LiliyaRafikova <liliya89fm@mail.ru>
2023-06-23 15:07:18 +0500

    Добавление функции show_help(): правила игры

--- src/help.py
+++ src/help.py
@@ -1,3 +1,13 @@
 """
 Раздел помощи.
 """
+import data
+
+
+def show_help() -> str:
+    """Выводит на экран правила игры"""
+    with open(data.HELP_PATH, 'r', encoding='utf-8') as file_in:
+        text_file = file_in.read()
+    print(text_file)
+    return text_file
+
