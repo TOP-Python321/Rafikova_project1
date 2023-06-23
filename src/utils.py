58ac8c3082ad3cb3da2e0de2424a5e35155aca5f
LiliyaRafikova <liliya89fm@mail.ru>
2023-06-23 14:26:15 +0500

    Добавление функции show_title(): заголовок игры

--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,16 @@
 """
 
 from configparser import ConfigParser
+from shutil import get_terminal_size
 import data
+
+
 def read_players() -> bool:
-    """Читает файл данных игроков, сохраняет информацию в соответствующую глобальную структуру данных. Возвращает True, если в файле данных игроков есть хотя бы одна запись, иначе False."""
+
+    """
+    Читает файл данных игроков, сохраняет информацию в соответствующую глобальную структуру данных. Возвращает True, если в файле данных игроков есть хотя бы одна запись, иначе False.
+    """
+
     config = ConfigParser()
     config.read(data.PLAYERS_PATH)
     config = {
@@ -18,7 +25,9 @@ def read_players() -> bool:
     data.players_db = config
     return bool(config)
 
+
 def write_players() -> None:
+
     """Записывает в файл данных игроков информацию из соответствующей глобальной структуры данных."""
     config = ConfigParser()
 
@@ -27,4 +36,18 @@ def write_players() -> None:
 
     with open(data.PLAYERS_PATH, "w", encoding="utf-8") as file_in:
         config.write(file_in)
+
+    return None
+
+
+def show_title() -> None:
+
+    """Выводит на экран заголовок игры при запуске программы"""
+    width = get_terminal_size().columns - 1
+    line1 = f'{"#" * width}'
+    line2 = f"\n#{' ' * (width-2)}#\n"
+    text_line = f'#{"Игра КРЕСТИКИ_НОЛИКИ".center(width-2)}#'
+    print(line1+line2+text_line+line2+line1)
+
+    return None
 
