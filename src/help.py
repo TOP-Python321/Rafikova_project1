5688a3e979feb6e441271ca75e871d1857b7f8e5
Gennadiy <renardes@gmail.com>
2023-06-24 11:24:15 +0500

    show_help(): комментарии и правки

--- src/help.py
+++ src/help.py
@@ -1,13 +1,16 @@
 """
 Раздел помощи.
 """
+
 import data
 
 
 def show_help() -> str:
     """Выводит на экран правила игры"""
+    # ИСПРАВИТЬ: может, всё же прочитать файл один раз заранее, а не во время каждого вызова функции?)
     with open(data.HELP_PATH, 'r', encoding='utf-8') as file_in:
         text_file = file_in.read()
+    # ИСПРАВИТЬ: и вывод, и возврат? я бы сказал: определитесь
     print(text_file)
     return text_file
 
