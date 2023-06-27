b53d88d4cd923f64944b35223a004ae7b6eef684
LiliyaRafikova <liliya89fm@mail.ru>
2023-06-27 15:02:25 +0500

    изменение функции на чтение файла

--- src/help.py
+++ src/help.py
@@ -4,13 +4,11 @@
 
 import data
 
-
-def show_help() -> str:
-    """Выводит на экран правила игры"""
+def read_rules() -> str:
+    """Чтение файла Правила игры"""
     # ИСПРАВИТЬ: может, всё же прочитать файл один раз заранее, а не во время каждого вызова функции?)
     with open(data.HELP_PATH, 'r', encoding='utf-8') as file_in:
         text_file = file_in.read()
     # ИСПРАВИТЬ: и вывод, и возврат? я бы сказал: определитесь
-    print(text_file)
     return text_file
 
