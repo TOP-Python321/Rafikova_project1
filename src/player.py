d43cee773256621750aff9ea7ce09516dca05295
LiliyaRafikova <liliya89fm@mail.ru>
2023-06-27 14:30:55 +0500

    функция name_input(): ввод имени игрока

--- src/player.py
+++ src/player.py
@@ -1,3 +1,11 @@
 """
 Работа с данными игроков.
 """
+import data
+
+def name_input() -> str:
+    while True:
+        name = input(f' {data.MESSAGES["ввод имени"]}{data.PROMPT}')
+        if data.NAME_PATTERN.fullmatch(name):
+            return name
+        print(f' {data.MESSAGES["некорректное имя"]} ')
